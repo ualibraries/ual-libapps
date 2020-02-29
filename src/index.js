6245dfa4284ae4ba529be2034e467e194cff289b
Will Simpson <wsimpson@email.arizona.edu>
2020-02-29 15:20:31 -0700

    Add #page div as container.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/vars.css";
 import "./css/normalize.css";
 import "./css/base.css";
+import "./css/grids.css";
 import "./css/components/links.css";
 import "./css/components/nav.css";
 import "./css/regions/header.css";
