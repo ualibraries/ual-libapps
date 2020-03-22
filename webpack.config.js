5189c342ec568db1b37ea1d0c6e2622b790d9378
Will Simpson <wsimpson@email.arizona.edu>
2020-03-22 15:05:13 -0700

    Fix config causing svgs to be exported twice.

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ const config = {
         ]
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
             loader: 'file-loader',
