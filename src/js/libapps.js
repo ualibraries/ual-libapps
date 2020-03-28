92c2dbfefca80d3ff63fee46784071dd6265bb60
Will Simpson <wsimpson@email.arizona.edu>
2020-03-28 14:02:04 -0700

    Remove LibApps specific "skip to content" link

--- src/js/libapps.js
+++ src/js/libapps.js
@@ -1,6 +1,7 @@
 (function($){
   $(document).ready(function(){
     $('html').addClass('sticky-footer ua-brand-icons overlay-menu-scroll-hide js')
+    $('#s-lg-public-skiplink').remove()
   });
 })(jQuery);
 
