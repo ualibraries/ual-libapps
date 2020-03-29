1cc984c6d0b63e06dd815b4627e90d1651df8936
Will Simpson <wsimpson@email.arizona.edu>
2020-03-28 19:34:37 -0700

    Fix order of CSS sheets.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import "./css/uaqs_navigation_overlay_menu_scroll.css";
-import "./css/ua_zen_back_to_top.css";
-import "./css/ua_zen_back_to_top.css";
 import "./ua-bootstrap/ua-bootstrap.min.css";
 import "./ua-brand-icons/ua-brand-icons.min.css";
 import "./css/styles.css";
+import "./css/uaqs_navigation_overlay_menu_scroll.css";
+import "./css/ua_zen_back_to_top.css";
