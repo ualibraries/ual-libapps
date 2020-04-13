cb8dc9705ac9796c70a90e632fd81885f277808f
Will Simpson <wsimpson@email.arizona.edu>
2020-04-12 23:56:56 -0700

    Add hack to change input placeholder text.

--- src/js/libapps.js
+++ src/js/libapps.js
@@ -2,6 +2,17 @@
   $(document).ready(function(){
     $('html').addClass('sticky-footer ua-brand-icons overlay-menu-scroll-hide js')
     $('#s-lg-public-skiplink').remove()
+    var count = 0;
+    var checkExist = setInterval(function() {
+        if (count >= 10) {
+            clearInterval(checkExist);
+        }
+        if ($('#s-la-content-search-query-11022').length) {
+            $('#s-la-content-search-query-11022').attr('placeholder', 'Search FAQ');
+            clearInterval(checkExist);
+        }
+        count++;
+    }, 10);
   });
 })(jQuery);
 
